a7112d75dd264477b7279a47cb8a83ef3a8b9dde
htew0001 <39622110+htew0001@users.noreply.github.com>
2026-02-28 04:27:36 +0000

    Deploying to gh-pages from @ htew0001/htew0001.github.io@a673d2536a05d253617ff8ebdf3713326e0d696e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,8 +448,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-one-1-work-accepted-in-iclr-2026-sparkles-smile",
+          title: 'One (1) work accepted in ICLR 2026. :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
